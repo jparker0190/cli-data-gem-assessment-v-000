f89c1eb6b8deb3d785308d7c31d880f98720452d
Joseph Parker <jparker010190@gmail.com>
2018-06-07 02:18:48 +0000

    adding encapisulation for Stock_Find class in CLI class

--- stock_scraper/lib/stock_scraper/cli.rb
+++ stock_scraper/lib/stock_scraper/cli.rb
@@ -1,5 +1,13 @@
 class StockScraper::CLI
   def call
-    puts "I live in the cli.rb file."
+    list_stocks
+  end
+  def list_stocks
+    puts " "
+    puts "Here are the biggest mover's in the market today:"
+    @stocks = StockScraper::Stock_Find.today
+    @stocks.each.with_index(1) do |equity, i|
+      puts"#{i}.#{equity.company}(#{equity.ticker}) - $#{equity.price}/#{equity.per_change}%".ljust(15)
+    end
   end
 end
--- stock_scraper/lib/stock_scraper/stock_find.rb
+++ stock_scraper/lib/stock_scraper/stock_find.rb
@@ -1,5 +1,24 @@
 class StockScraper::Stock_Find
-  def today
-    puts "I live in the stock_find.rb file."
+  attr_accessor :ticker, :company, :price, :per_change
+  def self.today
+    self.stock_scrape
+  end
+  def self.scrape
+    stocks = []
+    stocks << self.stock_scrape
+  end
+  def self.stock_scrape
+    stocks1 = self.new
+    stocks1.ticker = "FB"
+    stocks1.company = "Facebook"
+    stocks1.price = 198
+    stocks1.per_change = 56
+
+    stocks2 = self.new
+    stocks2.ticker = "AAPL"
+    stocks2.company = "Aaple, Inc."
+    stocks2.price = 197
+    stocks2.per_change = 47
+    [stocks1, stocks2]
   end
 end
